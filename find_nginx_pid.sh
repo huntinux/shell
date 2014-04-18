45c7eacfdd0ffdff9596fd689f653fae0faba43d
hongjin.cao <hongjin.cao@qunar.com>
2014-04-18 11:45:52 +0800

    add usage

--- find_nginx_pid.sh
+++ find_nginx_pid.sh
@@ -5,15 +5,20 @@
 # hongjin.cao (huntinux@gmail.com)
 # 2014-04-12
 
-#if [ $# -eq 1 ];then
-#	port=$1
-#else
-#	port=8080
-#fi
+function usage(){
+	echo "Usage:$0 [port]"
+	echo "port has default 8080"		
+}
 
-port=${1:-8080}
+if [ $# -eq 1 ];then
+	port=$1
+else
+	usage
+	port=8080
+fi
+#port=${1:-8080}
+
 nginxpid=`netstat -lpnt 2>/dev/null | grep $port | awk -F'/' '{print $(NF-1)}' | awk '{print $(NF)}'`
-
 if [ "$nginxpid" == "" ]; then
 	echo "nginx using port:[$port] does not exist."
 else
