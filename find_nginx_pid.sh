57a420393d52db93646fae44d27d47707eb876a3
hongjin.cao <hongjin.cao@qunar.com>
2014-04-18 11:38:21 +0800

    first commit

---
+++ find_nginx_pid.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# usage: killnginx [port] 
+# port has default value 8080
+# hongjin.cao (huntinux@gmail.com)
+# 2014-04-12
+
+#if [ $# -eq 1 ];then
+#	port=$1
+#else
+#	port=8080
+#fi
+
+port=${1:-8080}
+nginxpid=`netstat -lpnt 2>/dev/null | grep $port | awk -F'/' '{print $(NF-1)}' | awk '{print $(NF)}'`
+
+if [ "$nginxpid" == "" ]; then
+	echo "nginx using port:[$port] does not exist."
+else
+	echo "nginx port:[$port] pid:[$nginxpid]"
+fi
+
+#[ "$nginxpid" == "" ] && 
+#	echo "nginx using port:[$port] does not exist." ||
+#	echo "nginx port:[$port] pid:[$nginxpid]"
+
+exit 0
