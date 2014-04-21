f0c68442c7c8ef5656863b4e1ece58e48eb418eb
hongjin.cao <hongjin.cao@qunar.com>
2014-04-21 12:42:45 +0800

    add comment

--- readlink.sh
+++ readlink.sh
@@ -16,12 +16,14 @@ execuate(){
 #echo "dirname:$DIRNAME_RESULT"
 execuate dirname $0
 
+
 # 方法1： 使用readlink
+# readlink 跟随连接，能根据链接找到真正的文件
 #REAL_PATH1=$(dirname $(readlink -f $0))
 #echo "full path:$REAL_PATH1"
 execuate dirname $(readlink -f $0)
 
-# 方法2： 使用cd，pwd
+# 方法2： 使用cd，pwd, 得到当前文件路径，而不跟随链接
 #FULL_PATH=$(cd $(dirname $0);pwd)
 #echo \$FULL_PATH is $FULL_PATH
 execuate cd $(dirname $0) 
