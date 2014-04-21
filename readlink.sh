1837351026f13ccca4fbb022f8d2dd39154f4828
hongjin.cao <hongjin.cao@qunar.com>
2014-04-21 12:34:55 +0800

    2 method to read file path

--- readlink.sh
+++ readlink.sh
@@ -16,8 +16,13 @@ execuate(){
 #echo "dirname:$DIRNAME_RESULT"
 execuate dirname $0
 
+# 方法1： 使用readlink
 #REAL_PATH1=$(dirname $(readlink -f $0))
 #echo "full path:$REAL_PATH1"
 execuate dirname $(readlink -f $0)
 
-		
+# 方法2： 使用cd，pwd
+#FULL_PATH=$(cd $(dirname $0);pwd)
+#echo \$FULL_PATH is $FULL_PATH
+execuate cd $(dirname $0) 
+execuate pwd
