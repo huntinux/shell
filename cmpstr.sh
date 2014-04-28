f9409b08d3b9083398397bea9b010190ed7d5f02
hongjin.cao <hongjin.cao@qunar.com>
2014-04-28 11:26:02 +0800

    chp8,9

---
+++ cmpstr.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#
+# 比较字符串
+#
+
+#   =  as a test operator
+
+if [ "$string1" = "$string2" ]
+then
+   command
+fi
+
+#  if [ "X$string1" = "X$string2" ] is safer,
+#+ to prevent an error message should one of the variables be empty.
+#  (The prepended "X" characters cancel out.)
+
