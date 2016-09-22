fb39f164ba033c45c1d1c56d12a5e5f7745a1846
huntinux <Huntinux@gmail.com>
2016-09-22 16:32:19 +0800

    Update inotiry_rsync.sh

--- inotiry_rsync.sh
+++ inotiry_rsync.sh
@@ -9,26 +9,44 @@
 # variables
 #
 
-LOG_FILE=/dev/null
-WIKI_ROOT=/home/wiki/work/wiki/
-BACKUP_MACHINE_PATH=/home/wiki/work/wiki-backup/
-BACKUP_MACHINE_IP=127.0.0.1
-BACKUP_MACHINE_USER=wiki
+LOG_FILE=/home/wiki/backup-log/`date +'%Y-%m-%d'`.log
+WIKI_ROOT=/var/www/html/
+BACKUP_MACHINE_PATH=/home/manager/wiki-backup                             
+BACKUP_MACHINE_IP=10.0.0.1 
+BACKUP_MACHINE_USER=manager
 INOTIFYWAIT_BIN=`which inotifywait`
+
+#
+# function util
+#
+function LOG()
+{
+	echo "$@">>$LOG_FILE
+}
+
 
 #
 # start
 #
 
-cd ${WIKI_ROOT}                         
-$INOTIFYWAIT_BIN -mrq --excludei='(data/cache/|data/locks/|data/index/|data/tmp/)' --format  '%e %w%f' -e modify,create,delete,attrib,close_write,move . | \
-while read INO_EVENT INO_FILE
+LOG "=================================="
+LOG " Backup Start at:" `date +%H:%M:%S`
+LOG "=================================="
+
+cd $WIKI_ROOT || { LOG 'WIKI_ROOT ERROR' && exit; }
+$INOTIFYWAIT_BIN -mrq --excludei='(data/cache/|data/locks/|data/index/|data/tmp/)' --format  '%e %w%f %T' --timefmt='%Y-%m-%d/%H:%M:%S' -e modify,create,delete,attrib,close_write,move .  | \
+while read INO_EVENT INO_FILE INO_TIME
 do
-       echo "Event:$INO_EVENT File:$INO_FILE"
+       if [[ $INO_FILE =~ './data/meta/davcal.sqlite3' ]]; then
+		# DON'T LOG davcal things
+		:
+       else
+       		LOG "$INO_TIME Event:$INO_EVENT File:$INO_FILE"
+       fi
 
        if [[ $INO_EVENT =~ 'CREATE' ]] || [[ $INO_EVENT =~ 'MODIFY' ]] || [[ $INO_EVENT =~ 'CLOSE_WRITE' ]] || [[ $INO_EVENT =~ 'MOVED_TO' ]]         
        then
-       		rsync -avzc --recursive $(dirname ${INO_FILE}) ${BACKUP_MACHINE_USER}@${BACKUP_MACHINE_IP}:${BACKUP_MACHINE_PATH}          
+       		rsync -avzc  ${INO_FILE} ${BACKUP_MACHINE_USER}@${BACKUP_MACHINE_IP}:${BACKUP_MACHINE_PATH}          
        fi
 
        if [[ $INO_EVENT =~ 'DELETE' ]] || [[ $INO_EVENT =~ 'MOVED_FROM' ]]
@@ -40,7 +58,7 @@ do
        then
                	if [ ! -d "$INO_FILE" ] 
                	then
-               		rsync -avzc --recursive $(dirname ${INO_FILE}) ${BACKUP_MACHINE_USER}@${BACKUP_MACHINE_IP}:${BACKUP_MACHINE_PATH}
+               		rsync -avzcR $(dirname ${INO_FILE}) ${BACKUP_MACHINE_USER}@${BACKUP_MACHINE_IP}:${BACKUP_MACHINE_PATH}
                	fi
        fi
 done
